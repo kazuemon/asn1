10806cb4a8750bb4b0db590e761cefcb27aff9d8
kazuemon <git@k6n.jp>
2024-01-16 07:41:28 +0900

    🎨 change tab to space

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,63 @@
 import { TagClass, UniversalClassTag } from "./const";
 
 export type Asn1Data = {
-	id:
-		| {
-				tagType: number;
-				tagClass:
-					| typeof TagClass.APPLICATION
-					| typeof TagClass.CONTEXT_SPECIFIC
-					| typeof TagClass.PRIVATE;
-				isConstructed: boolean;
-		  }
-		| {
-				tagClass: typeof TagClass.UNIVERSAL;
-				tagType: UniversalClassTag;
-				isConstructed: boolean;
-		  };
-	len: number;
-	value: Asn1Data[] | Uint8Array;
+  id:
+    | {
+        tagType: number;
+        tagClass:
+          | typeof TagClass.APPLICATION
+          | typeof TagClass.CONTEXT_SPECIFIC
+          | typeof TagClass.PRIVATE;
+        isConstructed: boolean;
+      }
+    | {
+        tagClass: typeof TagClass.UNIVERSAL;
+        tagType: UniversalClassTag;
+        isConstructed: boolean;
+      };
+  len: number;
+  value: Asn1Data[] | Uint8Array;
 };
 
 export const decodeAsn1 = (aryBuf: ArrayBuffer): Asn1Data => {
-	return _decodeAsn1(new Uint8Array(aryBuf))[0];
+  return _decodeAsn1(new Uint8Array(aryBuf))[0];
 };
 
 export const _decodeAsn1 = (uint8Ary: Uint8Array): Asn1Data[] => {
-	if (uint8Ary.length < 2) throw new RangeError();
-	const result: Asn1Data[] = [];
-	for (let i = 0; i < uint8Ary.length; i++) {
-		// Identifer Octets
-		const idOcts = uint8Ary[i];
-		const tagType = idOcts & 31;
-		const isConstructed = (idOcts & 32) >> 5 === 1;
-		const tagClass = (idOcts >> 6) as TagClass;
-		// Length Octets
-		const lenOcts = uint8Ary[++i];
-		const len = (lenOcts << 1) >> 1; // FIXME: https://en.wikipedia.org/wiki/X.690#Length_octets
-		// Value Octets
-		const valueOcts = uint8Ary.slice(++i, i + len);
-		if (valueOcts.length !== len) throw new RangeError();
-		const value = isConstructed ? _decodeAsn1(valueOcts) : valueOcts;
-		result.push({
-			id:
-				tagClass === TagClass.UNIVERSAL
-					? {
-							tagType: tagType as UniversalClassTag,
-							isConstructed,
-							tagClass,
-					  }
-					: {
-							isConstructed,
-							tagClass,
-							tagType,
-					  },
-			len,
-			value,
-		});
-		i += len - 1;
-	}
-	return result;
+  if (uint8Ary.length < 2) throw new RangeError();
+  const result: Asn1Data[] = [];
+  for (let i = 0; i < uint8Ary.length; i++) {
+    // Identifer Octets
+    const idOcts = uint8Ary[i];
+    const tagType = idOcts & 31;
+    const isConstructed = (idOcts & 32) >> 5 === 1;
+    const tagClass = (idOcts >> 6) as TagClass;
+    // Length Octets
+    const lenOcts = uint8Ary[++i];
+    const len = (lenOcts << 1) >> 1; // FIXME: https://en.wikipedia.org/wiki/X.690#Length_octets
+    // Value Octets
+    const valueOcts = uint8Ary.slice(++i, i + len);
+    if (valueOcts.length !== len) throw new RangeError();
+    const value = isConstructed ? _decodeAsn1(valueOcts) : valueOcts;
+    result.push({
+      id:
+        tagClass === TagClass.UNIVERSAL
+          ? {
+              tagType: tagType as UniversalClassTag,
+              isConstructed,
+              tagClass,
+            }
+          : {
+              isConstructed,
+              tagClass,
+              tagType,
+            },
+      len,
+      value,
+    });
+    i += len - 1;
+  }
+  return result;
 };
 
 // export const encodeAsn1 = (asn1Data: Asn1Data): ArrayBuffer => {
