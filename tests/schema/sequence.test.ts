ea0c0cce19a95dfad9811b22928042ffd0f65811
kazuemon <git@k6n.jp>
2024-01-20 07:42:09 +0900

    🧪 Add failing tests for development

--- tests/schema/sequence.test.ts
+++ tests/schema/sequence.test.ts
@@ -3,6 +3,7 @@ import { decodeAsn1 } from "../../src";
 import { integer } from "../../src/schema/integer";
 import { octetString } from "../../src/schema/octet-string";
 import { sequence } from "../../src/schema/sequence";
+import { TagClass } from "../../src/const";
 
 describe("Sequence", () => {
   /**
@@ -81,66 +82,239 @@ describe("Sequence", () => {
     });
   });
   describe("optional", () => {
+    describe("[required, optional]", () => {
+      const schema = sequence({
+        fields: [
+          {
+            name: "id",
+            schema: integer(),
+          },
+          {
+            name: "message",
+            schema: octetString(),
+            optional: true,
+          },
+        ],
+      });
+
+      describe("Data contain optional fields", () => {
+        it("encode", () => {
+          expect(
+            Buffer.from(
+              schema.encode({
+                id: 50,
+                message: "hello",
+              }),
+            )
+              .toString("hex")
+              .toUpperCase(),
+          ).toEqual("300A020132040568656C6C6F");
+        });
+
+        it("decode", () => {
+          const result = schema.decode(
+            decodeAsn1(Buffer.from("300a020132040568656c6c6f", "hex")),
+          );
+
+          expect(result).toEqual({
+            id: 50,
+            message: "hello",
+          });
+        });
+      });
+
+      describe("Data does not contain optional fields", () => {
+        it("encode", () => {
+          expect(
+            Buffer.from(
+              schema.encode({
+                id: 50,
+              }),
+            )
+              .toString("hex")
+              .toUpperCase(),
+          ).toEqual("3003020132");
+        });
+
+        it("decode", () => {
+          const result = schema.decode(
+            decodeAsn1(Buffer.from("3003020132", "hex")),
+          );
+
+          expect(result).toEqual({
+            id: 50,
+          });
+        });
+      });
+    });
+    describe("[optional, required]", () => {
+      const schema = sequence({
+        fields: [
+          {
+            name: "id",
+            schema: integer(),
+            optional: true,
+          },
+          {
+            name: "message",
+            schema: octetString(),
+          },
+        ],
+      });
+
+      describe("Data contain optional fields", () => {
+        it("decode", () => {
+          const result = schema.decode(
+            decodeAsn1(Buffer.from("300a020132040568656c6c6f", "hex")),
+          );
+
+          expect(result).toEqual({
+            id: 50,
+            message: "hello",
+          });
+        });
+      });
+
+      describe("Data does not contain optional fields", () => {
+        it("encode", () => {
+          expect(
+            Buffer.from(
+              schema.encode({
+                message: "hello",
+              }),
+            )
+              .toString("hex")
+              .toUpperCase(),
+          ).toEqual("3007040568656C6C6F");
+        });
+
+        it("decode", () => {
+          const result = schema.decode(
+            decodeAsn1(Buffer.from("3007040568656c6c6f", "hex")),
+          );
+
+          expect(result).toEqual({
+            message: "hello",
+          });
+        });
+      });
+    });
+  });
+  describe("Context-Specific", () => {
+    // from https://letsencrypt.org/ja/docs/a-warm-welcome-to-asn1-and-der
     const schema = sequence({
       fields: [
         {
-          name: "id",
-          schema: integer(),
+          name: "x",
+          schema: integer({
+            tagClass: TagClass.CONTEXT_SPECIFIC,
+            tagType: 0,
+          }),
+          optional: true,
         },
         {
-          name: "message",
-          schema: octetString(),
+          name: "y",
+          schema: integer({
+            tagClass: TagClass.CONTEXT_SPECIFIC,
+            tagType: 1,
+          }),
           optional: true,
         },
       ],
     });
+    describe("none", () => {
+      it("encode", () => {
+        expect(
+          Buffer.from(schema.encode({})).toString("hex").toUpperCase(),
+        ).toEqual("3000");
+      });
 
-    describe("Data contain optional fields", () => {
+      it("decode", () => {
+        const result = schema.decode(decodeAsn1(Buffer.from("3000", "hex")));
+
+        expect(result).toEqual({});
+      });
+    });
+    describe("only x", () => {
       it("encode", () => {
         expect(
           Buffer.from(
             schema.encode({
-              id: 50,
-              message: "hello",
+              x: 9,
             }),
           )
             .toString("hex")
             .toUpperCase(),
-        ).toEqual("300A020132040568656C6C6F");
+        ).toEqual("3003800109");
       });
 
       it("decode", () => {
         const result = schema.decode(
-          decodeAsn1(Buffer.from("300a020132040568656c6c6f", "hex")),
+          decodeAsn1(Buffer.from("3003800109", "hex")),
         );
 
         expect(result).toEqual({
-          id: 50,
-          message: "hello",
+          x: 9,
         });
       });
     });
-
-    describe("Data does not contain optional fields", () => {
+    describe("only y", () => {
       it("encode", () => {
         expect(
           Buffer.from(
             schema.encode({
-              id: 50,
+              y: 10,
             }),
           )
             .toString("hex")
             .toUpperCase(),
-        ).toEqual("3003020132");
+        ).toEqual("300381010A");
       });
 
       it("decode", () => {
         const result = schema.decode(
-          decodeAsn1(Buffer.from("3003020132", "hex")),
+          decodeAsn1(Buffer.from("300381010A", "hex")),
         );
 
         expect(result).toEqual({
-          id: 50,
+          y: 10,
+        });
+      });
+    });
+    describe("x and y", () => {
+      it("encode", () => {
+        expect(
+          Buffer.from(
+            schema.encode({
+              x: 9,
+              y: 10,
+            }),
+          )
+            .toString("hex")
+            .toUpperCase(),
+        ).toEqual("300680010981010A");
+      });
+
+      it("decode", () => {
+        const result = schema.decode(
+          decodeAsn1(Buffer.from("300680010981010A", "hex")),
+        );
+
+        expect(result).toEqual({
+          x: 9,
+          y: 10,
+        });
+      });
+    });
+    describe("y and x", () => {
+      it("decode", () => {
+        const result = schema.decode(
+          decodeAsn1(Buffer.from("300681010A800109", "hex")),
+        );
+
+        expect(result).toEqual({
+          x: 9,
+          y: 10,
         });
       });
     });
