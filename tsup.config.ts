95d6e76d04e906fdd6ee87fd43d2825cc64f02df
kazuemon <git@k6n.jp>
2024-11-20 23:00:18 +0900

    🔧 enable minify

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["./src/index.ts"],
   clean: true,
   format: ["cjs", "esm"],
-  minify: false,
+  minify: true,
   dts: true,
   outDir: "./dist",
   treeshake: true,
